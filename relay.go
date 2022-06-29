5c5036d98e632464ee8e83743bec4adbbdd27e33
togls <tomls@tuta.io>
2022-06-29 10:51:31 +0800

    fix relay deadline wait 5 second to read deadline

--- relay.go
+++ relay.go
@@ -6,38 +6,42 @@ import (
 	"io"
 	"net"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/togls/trojan-go/log"
 )
 
 // relay copies between left and right bidirectionally
-func relay(left, right net.Conn) error {
-	var received, sent int64
+func relay(left, right net.Conn) (received, sent int64, err error) {
 	var rerr, serr error
+	wait := 5 * time.Second
+	wg := &sync.WaitGroup{}
 
+	wg.Add(1)
 	go func() {
-		received, rerr = io.Copy(right, left)
-		right.SetReadDeadline(time.Now())
+		defer wg.Done()
+
+		received, rerr = io.Copy(left, right)
+		left.SetReadDeadline(time.Now().Add(wait))
 	}()
 
-	sent, serr = io.Copy(left, right)
-	left.SetReadDeadline(time.Now())
+	sent, serr = io.Copy(right, left)
+	right.SetReadDeadline(time.Now().Add(wait))
 
-	if rerr != nil && !errors.Is(rerr, os.ErrDeadlineExceeded) {
-		return fmt.Errorf("relay left->right err %w", rerr)
-	}
+	wg.Wait()
 
 	if serr != nil && !errors.Is(serr, os.ErrDeadlineExceeded) {
-		return fmt.Errorf("relay right->left err %w", serr)
+		err = fmt.Errorf("left->right: %w", serr)
+		return
 	}
 
-	log.Info().
-		Int64("received", received).
-		Int64("sent", sent).
-		Msg("relay success")
+	if rerr != nil && !errors.Is(rerr, os.ErrDeadlineExceeded) {
+		err = fmt.Errorf("right->left: %w", rerr)
+		return
+	}
 
-	return nil
+	return
 }
 
 func relayUDP(left, right net.PacketConn) error {
