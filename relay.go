4ffea7defb9ea7ef88db3086bb9066c3bdb4e293
togls <tomls@tuta.io>
2022-07-21 14:04:31 +0800

    chore: remove unused code

--- relay.go
+++ relay.go
@@ -8,8 +8,6 @@ import (
 	"os"
 	"sync"
 	"time"
-
-	"github.com/togls/trojan-go/log"
 )
 
 // relay copies between left and right bidirectionally
@@ -41,32 +39,4 @@ func relay(left, right net.Conn) (received, sent int64, err error) {
 	}
 
 	return
-}
-
-func relayUDP(left, right net.PacketConn) error {
-	var received, sent int64
-	var rerr, serr error
-
-	go func() {
-		// received, rerr = io.Copy(right, left)
-		right.SetReadDeadline(time.Now())
-	}()
-
-	// sent, serr = io.Copy(left, right)
-	left.SetReadDeadline(time.Now())
-
-	if rerr != nil && !errors.Is(rerr, os.ErrDeadlineExceeded) {
-		return fmt.Errorf("relay left->right err %w", rerr)
-	}
-
-	if serr != nil && !errors.Is(serr, os.ErrDeadlineExceeded) {
-		return fmt.Errorf("relay right->left err %w", serr)
-	}
-
-	log.Info().
-		Int64("received", received).
-		Int64("sent", sent).
-		Msg("relay success")
-
-	return nil
 }
